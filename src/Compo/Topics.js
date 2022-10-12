f95a46dd88482ac82a6989b5e9cb247ef527836f
Shihab585203 <freelancersehab1@gmail.com>
2022-10-12 21:23:17 +0600

    modified section

--- src/Compo/Topics.js
+++ src/Compo/Topics.js
@@ -11,14 +11,15 @@ const Topics = () => {
         <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-10 md:py-10 md:px-10 lg:px-32 text-gray-900">
           <img
             src="https://images.unsplash.com/photo-1585432959315-d9342fd58eb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            className="w-3/4 drop-shadow-2xl rounded-lg"
+            className="drop-shadow-2xl rounded-lg"
             alt=""
           />
           <h1 className="text-4xl font-bold leading-none sm:text-5xl xl:max-w-3xl mt-7 text-gray-800">
             Welcome To Mega Quiz Ground
           </h1>
           <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl text-gray-900">
-            Best Quiz Platform For Programming Hero. The Best Quiz Website for Web Developers.
+            Best Quiz Platform For Programming Hero. The Best Quiz Website for
+            Web Developers.
           </p>
         </div>
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
